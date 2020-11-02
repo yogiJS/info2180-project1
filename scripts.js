9faeaa8f8be6a26ae201ddff8db3e3d18460dd22
Renford Cohen <renford.cohenjr@gmail.com>
2020-11-01 23:51:31 -0500

    finished

--- scripts.js
+++ scripts.js
@@ -1,1 +1,12 @@
 /* Add your JavaScript to this file */
+
+const email = document.getElementById('email')
+const form = document.querySelector('form')
+
+form.addEventListener('submit', (e) => {
+ if (email.nodeValue === ""){
+     document.getElementById('message').innerHTML = 'Please enter an email'
+ }
+
+
+})
